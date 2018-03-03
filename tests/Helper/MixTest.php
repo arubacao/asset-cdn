ec05610115c1819d2d5a7bb0a07943ba95eb301b
arubacao <arubacao@gmail.com>
2018-03-03 21:04:48 +0100

    implement MixTest

---
+++ tests/Helper/MixTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Arubacao\AssetCdn\Test\Helper;
+
+
+use Arubacao\AssetCdn\Test\TestCase;
+use Illuminate\Support\Facades\Log;
+
+class MixTest extends TestCase
+{
+    /** @test */
+    public function mix_cdn_falls_back_to_mix_if_disabled()
+    {
+        $this->app['config']->set('asset-cdn.use_cdn', false);
+        $url = mix_cdn('js/back.app.js');
+        $this->assertInstanceOf('Illuminate\Support\HtmlString', $url);
+        $this->assertSame('/js/back.app.js?id=0dd41baa9f9a73f1dd97', $url->toHtml());
+    }
+
+    /** @test */
+    public function mix_cdn_returns_correct_url()
+    {
+        $url = mix_cdn('js/back.app.js');
+        $this->assertInstanceOf('Illuminate\Support\HtmlString', $url);
+        $this->assertSame('http://cdn.localhost/js/back.app.js?id=0dd41baa9f9a73f1dd97', $url->toHtml());
+    }
+
+    /** @test */
+    public function mix_cdn_reports_exception_with_unknown_file()
+    {
+        Log::shouldReceive('error')
+            ->once()
+            ->withArgs(
+                function ($message) {
+                    $this->assertSame("Unable to locate Mix file: /js/unknown.app.js.", $message);
+                    return true;
+                }
+            )
+            ->andReturnNull();
+        mix_cdn('js/unknown.app.js');
+    }
+
+    /** @test */
+    public function mix_cdn_throws_exception_with_no_manifest_file()
+    {
+        $this->app->bind('path.public', function () {
+            return __DIR__.'/../testfiles/dummy';
+        });
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('The Mix manifest does not exist.');
+
+        mix_cdn('js/unknown.app.js');
+    }
+}
