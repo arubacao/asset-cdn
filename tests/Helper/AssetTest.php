d92ecc396e9ff6e32c9f951cbd82f86c2b303a42
arubacao <arubacao@gmail.com>
2018-03-03 21:04:58 +0100

    implement AssetTest

---
+++ tests/Helper/AssetTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Arubacao\AssetCdn\Test\Helper;
+
+
+use Arubacao\AssetCdn\Test\TestCase;
+use Illuminate\Support\Facades\Log;
+
+class AssetTest extends TestCase
+{
+    /** @test */
+    public function asset_cdn_falls_back_to_asset_if_disabled()
+    {
+        $this->app['config']->set('asset-cdn.use_cdn', false);
+        $urls = [
+            asset_cdn('images/auth-background.jpg'),
+            asset_cdn('/images/auth-background.jpg')
+        ];
+        foreach ($urls as $url) {
+            $this->assertSame('http://localhost/images/auth-background.jpg', $url);
+        }
+    }
+
+    /** @test */
+    public function asset_cdn_returns_correct_url()
+    {
+        $urls = [
+            asset_cdn('images/auth-background.jpg'),
+            asset_cdn('/images/auth-background.jpg')
+        ];
+        foreach ($urls as $url) {
+            $this->assertSame('http://cdn.localhost/images/auth-background.jpg', $url);
+        }
+    }
+}
