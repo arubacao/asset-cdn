2ea36608b1d8b3d722602cea604d6c05f5588ba8
arubacao <arubacao@gmail.com>
2018-03-03 21:27:56 +0100

    edit config

--- config/asset-cdn.php
+++ config/asset-cdn.php
@@ -7,21 +7,16 @@ return [
     'cdn_url' => '',
 
     'filesystem' => [
-
         'disk' => 'asset-cdn',
 
         'options' => [
-            'ACL' => 'public-read',
-            'CacheControl' => 'max-age=31536000, public',
+            //
         ],
     ],
 
     'files' => [
-
-        // Excludes "hidden" directories and files (starting with a dot).
         'ignoreDotFiles' => true,
 
-        // Forces the finder to ignore version control directories.
         'ignoreVCS' => true,
 
         'include' => [
